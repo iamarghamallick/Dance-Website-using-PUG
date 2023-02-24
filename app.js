10a47efa56cb7866ab92cd169a7a95e3b458177c
Argha Mallick <arghamallick5@gmail.com>
2023-02-24 19:11:21 +0530

    contact form data stored in the mongodb database

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const path = require('path');
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
 const app = express();
 const port = 5000;
+
+main().catch(err => console.log(err));
+async function main() {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect('mongodb://127.0.0.1:27017/contactDance');
+}
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    concern: String
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
@@ -13,13 +31,18 @@ app.set('views', path.join(__dirname, 'views')) // Set the views directory
 
 // ENDPOINTS
 app.get('/', (req, res) => {
-    const params = {  }
+    const params = {}
     res.status(200).render('home.pug', params);
 })
-
 app.get('/contact', (req, res) => {
-    const params = {  }
+    const params = {}
     res.status(200).render('contact.pug', params);
+})
+
+app.post('/contact', (req, res) => {
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{res.send("Information saved successfully")}).catch(()=>{res.status(400).send("Failed to saved data")});
+    // res.status(200).render('contact.pug');
 })
 
 // START THE SERVER
